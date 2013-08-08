08764de014095ab17b423dadcea4d9d472609d8d
Nathan Douglas <ndouglas@devontechnologies.com>
2013-08-08 11:20:27 -0500

    Initial Commit

---
+++ NDDStateMachine/NDDStateMachine.h
@@ -0,0 +1,13 @@
+//
+//  NDDStateMachine.h
+//  NDDStateMachine
+//
+//  Created by Nathan Douglas on 08/08/2013.
+//  Copyright (c) 2013 Nathan Douglas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NDDStateMachine : NSObject
+
+@end
---
+++ NDDStateMachine/NDDStateMachine.m
@@ -0,0 +1,13 @@
+//
+//  NDDStateMachine.m
+//  NDDStateMachine
+//
+//  Created by Nathan Douglas on 08/08/2013.
+//  Copyright (c) 2013 Nathan Douglas. All rights reserved.
+//
+
+#import "NDDStateMachine.h"
+
+@implementation NDDStateMachine
+
+@end
